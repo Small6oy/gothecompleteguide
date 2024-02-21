7437161aff58cc0ccfd69cc4b1f7c47732f6697b
Small6oy <gggndlovu@gmail.com>
2024-02-21 13:05:28 +1100

    Got API working

--- api/main.go
+++ api/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
+	"example.com/api/db"
 	"example.com/api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDb()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/event", createEvent)
 	server.DELETE("/event", delEvent)
 	server.PUT("/event", updateEvent)
@@ -18,8 +22,29 @@ func main() {
 	server.Run(":8080")
 }
 
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could get Id"})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not fetch requested data"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"event": event,
+	})
+}
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not fetch requested data"})
+		return
+	}
 	context.JSON(http.StatusOK, gin.H{
 		"events": events,
 	})
@@ -33,7 +58,11 @@ func createEvent(context *gin.Context) {
 		return
 	}
 
-	event.Save()
+	err = event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event. Try again later."})
+		return
+	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
--- api/models/event.go
+++ api/models/event.go
@@ -1,9 +1,14 @@
 package models
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"example.com/api/db"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding: "required"`
 	Description string    `binding: "required"`
 	Location    string    `binding: "required"`
@@ -13,14 +18,64 @@ type Event struct {
 
 var events = []Event{}
 
-func (e Event) Save() {
-	events = append(events, e)
+func (e Event) Save() error {
+	query := `
+		INSERT INTO events(name, description, location, dateTime, userID)
+		VALUES (?, ?, ?, ?, ?)`
+
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		fmt.Println("Failed to Prepare Statement", err)
+		return err
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		fmt.Println("Failed to Exec Statement", err)
+		return err
+	}
+
+	fmt.Println(result)
+	id, err := result.LastInsertId()
+	e.ID = id
+
+	return err
+}
+
+func GetAllEvents() ([]Event, error) {
+	query := `SELECT * FROM events`
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var events []Event
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+
+	return events, nil
+}
+
+func GetEventById(eventId int64) (*Event, error) {
+	query := `SELECT * FROM events WHERE id = ?`
+	row := db.DB.QueryRow(query, eventId)
+
+	var event Event
+	err := row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+	if err != nil {
+		return nil, err
+	}
+	return &event, nil
 }
 
 func (e Event) Remove() {
 	events = append(events, e)
-}
-
-func GetAllEvents() []Event {
-	return events
 }
